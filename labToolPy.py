117059eb88c14795af8728b708c6379a51e70f0b
smejaNemo <smeja.nemo@yandex.com>
2017-07-29 23:53:31 +0200

    raw

--- labToolPy.py
+++ labToolPy.py
@@ -7,3 +7,25 @@ Created on Sat Jul 29 22:49:36 2017
 """
 
 import numpy as np
+
+class ThMotor:
+    def __init__(self):
+        self.code = ''
+    def MoveTo(position):
+        command = 'MOVE ' + str(position) + '|'
+        self.code = command
+        return command
+
+class LabTool:
+    def __init__(self):
+        self.code = ''
+        self.thDev = ThMotor()
+    def Thorlabs(self,existing = True):
+        
+        self.code = self.code + thDev.MoveTo
+    def run (self):
+        print(self.code)
+
+l = LabTool()
+l.thDev.MoveTo(0)
+        
