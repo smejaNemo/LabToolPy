f84cdd067c0860616f4071fc294a15ec9381ab80
smejaNemo <smeja.nemo@yandex.com>
2017-08-05 21:35:09 +0200

    Last Version

--- labToolPy.py
+++ labToolPy.py
@@ -8,24 +8,97 @@ Created on Sat Jul 29 22:49:36 2017
 
 import numpy as np
 
-class ThMotor:
+
+
+class ProgNote:
     def __init__(self):
-        self.code = ''
-    def MoveTo(position):
+        self.code = []
+    def addCommand(self,command):
+        self.code.append(command)
+    def delCommand(self):
+        del self.code[-1]
+    def printCommands(self):
+        for l in self.code:
+            print(l)
+    def genOutputLine(self):
+        outLine = ''
+        for l in self.code:
+            outLine += l
+        return outLine
+    def run(self,repetitions):
+        outLine = ''
+        for l in self.code:
+            outLine += l
+        for i in range(repetitions):
+            print(outLine)
+    def check(self):
+        ok = True
+        return ok
+    
+
+class ThMotor:
+    def __init__(self,notebook):
+        self.notebook = notebook
+    def MoveTo(self,position):
         command = 'MOVE ' + str(position) + '|'
-        self.code = command
+        self.notebook.addCommand(command)
+        return command
+    def SelectDev(self,devNumber):
+        command = 'SELECT_DRV ' + str(devNumber) + '|'
+        self.notebook.addCommand(command)
+        return command
+    def SetVelocity(self,velocity):
+        command = 'VELOCITY ' + str(velocity) + '|'
+        self.notebook.addCommand(command)
+        return command
+    def Move(self,value):
+        """value <+|-> example: Mevo(+5) go to current position +/- value"""
+        command = 'MOVE ' + str(velocity) + '|'
+        self.notebook.addCommand(command)
+        return command
+    
+
+class LecOsc:
+    def __init__(self,notebook):
+        self.notebook = notebook
+    def OscConnect(self,ipAdress):
+        command = 'OSCI_CONNECT ' + str(ipAdress) + '|'
+        self.notebook.addCommand(command)
+        return command
+    def OscDisconnct(self):
+        command = 'OSCI_DISCONNECT ' + '|'
+        self.notebook.addCommand(command)
+        return command 
+    def OscScreen(self):
+        command = 'OSCI_SCREEN ' + '|'
+        self.notebook.addCommand(command)
+        return command 
+    def OscStore(self,fileName):
+        command = 'OSCI_STORE ' + str(fileName) + '|'
+        self.notebook.addCommand(command)
+        return command
+    def OscCmd(self,cmd):
+        command = 'OSCI_CMD' + str(cmd) + '|'
+        self.notebook.addCommand(command)
         return command
 
 class LabTool:
     def __init__(self):
+        self.notebook = ProgNote()
         self.code = ''
-        self.thDev = ThMotor()
-    def Thorlabs(self,existing = True):
-        
-        self.code = self.code + thDev.MoveTo
-    def run (self):
-        print(self.code)
+        self.ThDev = ThMotor(self.notebook)
+        self.LecOsc = LecOsc(self.notebook)
+    def run (self,repetitions):
+        self.notebook.run(repetitions)
 
 l = LabTool()
-l.thDev.MoveTo(0)
+l.ThDev.MoveTo(0)
+l.ThDev.MoveTo(10)
+l.ThDev.MoveTo(100)
+l.LecOsc.OscScreen()
+l.LecOsc.OscConnect('192.168.1.104')
+l.notebook.printCommands()
+#print(l.notebook.genOutputLine().split('|'))
+l.notebook.run(10)
+help(l.ThDev.Move)
         
